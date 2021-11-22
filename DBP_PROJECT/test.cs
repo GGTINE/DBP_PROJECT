2381e989fa4094a533fb18f78eb550fa86a80851
병일 김 <wqpo1235@naver.com>
2021-11-22 09:16:29 +0900

    테스트 추가

---
+++ DBP_PROJECT/test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBP_PROJECT
+{
+    public partial class test : Form
+    {
+        public test()
+        {
+            InitializeComponent();
+        }
+
+        private void 저장SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("저장 클릭했습니다.");
+        }
+    }
+}
